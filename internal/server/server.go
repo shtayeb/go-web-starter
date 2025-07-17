4d0d4018b4ffe73332e748be8dc7c9bcfca3db83
Shahryar Tayeb <shahryar.dev@gmail.com>
2025-07-17 18:11:15 +0900

    wip

--- internal/server/server.go
+++ internal/server/server.go
@@ -10,20 +10,36 @@ import (
 	_ "github.com/joho/godotenv/autoload"
 
 	"go-htmx-sqlite/internal/database"
+	"go-htmx-sqlite/internal/mailer"
 )
 
 type Server struct {
-	port int
-
-	db database.Service
+	port   int
+	db     database.Service
+	mailer mailer.Mailer
 }
 
 func NewServer() *http.Server {
 	port, _ := strconv.Atoi(os.Getenv("PORT"))
-	NewServer := &Server{
-		port: port,
 
-		db: database.New(),
+	smtp := struct {
+		host     string
+		port     int
+		username string
+		password string
+		sender   string
+	}{
+		host:     os.Getenv("SMTP_HOST"),
+		port:     func() int { p, _ := strconv.Atoi(os.Getenv("SMTP_PORT")); return p }(),
+		username: os.Getenv("SMTP_USERNAME"),
+		password: os.Getenv("SMTP_PASSWORD"),
+		sender:   os.Getenv("SMTP_SENDER"),
+	}
+
+	NewServer := &Server{
+		port:   port,
+		db:     database.New(),
+		mailer: mailer.New(smtp.host, smtp.port, smtp.username, smtp.password, smtp.sender),
 	}
 
 	// Declare Server config
