9c2d9023842d60989fc94d17cee53067aebb0caa
Shahryar Tayeb <shahryar.dev@gmail.com>
2025-08-20 11:17:33 +0900

    new tests internal package

--- internal/tests/e2e_test.go
+++ internal/tests/e2e_test.go
@@ -1,20 +1,11 @@
-package server
+package tests
 
 import (
 	"fmt"
-	"go-web-starter/internal/config"
-	"go-web-starter/internal/database"
-	"go-web-starter/internal/jsonlog"
-	"go-web-starter/internal/mailer"
-	"go-web-starter/internal/queries"
 	"net/http"
-	"net/http/httptest"
-	"os"
 	"strings"
 	"testing"
 	"time"
-
-	"github.com/joho/godotenv"
 )
 
 func TestE2E(t *testing.T) {
@@ -58,42 +49,6 @@ func TestE2E(t *testing.T) {
 	t.Run("Static Assets", func(t *testing.T) {
 		testStaticAssets(t, client, baseURL)
 	})
-}
-
-// startTestServer starts a test HTTP server for testing
-func startTestServer(t *testing.T) *httptest.Server {
-	if err := godotenv.Load("../../.env.test"); err != nil {
-		t.Logf("Warning: Could not load .env.test file: %v", err)
-	}
-
-	cfg := config.LoadConfigFromEnv()
-
-	// Try to connect to database, if it fails, skip the test
-	dbService := database.New(cfg.Database)
-	if dbService == nil {
-		return nil
-	}
-
-	health := dbService.Health()
-	if health["status"] != "up" {
-		t.Logf("Database is not available: %s", health["error"])
-		return nil
-	}
-
-	sqlDB := dbService.GetDB()
-
-	server := NewServer(
-		cfg,
-		dbService,
-		queries.New(sqlDB),
-		jsonlog.New(os.Stdout, jsonlog.LevelInfo),
-		mailer.New(cfg.Mailer),
-		NewSessionManager(sqlDB),
-	)
-
-	srv := httptest.NewServer(server.RegisterRoutes())
-
-	return srv
 }
 
 // testHealthEndpoint tests the health check endpoint
