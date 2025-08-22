f4445621ee655a6adaaa49e708bc5a24cc93d55e
Shahryar Tayeb <shahryar.dev@gmail.com>
2025-08-22 11:25:59 +0900

    templui sidebar implementation

--- cmd/web/components/ui/icon/icondata.go
+++ cmd/web/components/ui/icon/icondata.go
@@ -1,4 +1,4 @@
-// templui component icon - version: v0.90.0 installed by templui v0.90.0
+// templui component icon - version: v0.92.0 installed by templui v0.92.0
 package icon
 
 // This file is auto generated
