cff7bc50ee24a322591d64e2d365a67af63be7bf
Shahryar Tayeb <shahryar.dev@gmail.com>
2025-09-03 09:59:00 +0900

    refactor server command

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,16 +1,7 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"log"
-	"net/http"
-	"os/signal"
-	"syscall"
-	"time"
-
 	"go-web-starter/cmd/api/commands"
-	"go-web-starter/internal/server"
 
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/spf13/cobra"
@@ -22,63 +13,13 @@ func main() {
 		Short: "A web application built with Go.",
 	}
 
-	serverCmd := &cobra.Command{
-		Use:   "server",
-		Short: "Start the web server",
-		Run:   execServerCommand,
-	}
-
 	// CLI commands
 	rootCmd.AddCommand(
-		serverCmd,
+		commands.ServerCommand(),
 		commands.SeedCommand(),
 		commands.PingCommand(),
 		commands.MigrateCommand(),
 	)
 
 	rootCmd.Execute()
-}
-
-func execServerCommand(cmd *cobra.Command, args []string) {
-	server := server.NewHttpServer()
-
-	// Create a done channel to signal when the shutdown is complete
-	done := make(chan bool, 1)
-
-	// Run graceful shutdown in a separate goroutine
-	go gracefulShutdown(server, done)
-
-	err := server.ListenAndServe()
-	if err != nil && err != http.ErrServerClosed {
-		panic(fmt.Sprintf("http server error: %s", err))
-	}
-
-	// Wait for the graceful shutdown to complete
-	<-done
-	log.Println("Graceful shutdown complete.")
-}
-
-func gracefulShutdown(apiServer *http.Server, done chan bool) {
-	// Create context that listens for the interrupt signal from the OS.
-	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
-	defer stop()
-
-	// Listen for the interrupt signal.
-	<-ctx.Done()
-
-	log.Println("shutting down gracefully, press Ctrl+C again to force")
-	stop() // Allow Ctrl+C to force shutdown
-
-	// The context is used to inform the server it has 5 seconds to finish
-	// the request it is currently handling
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	if err := apiServer.Shutdown(ctx); err != nil {
-		log.Printf("Server forced to shutdown with error: %v", err)
-	}
-
-	log.Println("Server exiting")
-
-	// Notify the main goroutine that the shutdown is complete
-	done <- true
 }
