9be758370d0e6ea735a6f86f47681352a94ca779
Shahryar Tayeb <shahryar.dev@gmail.com>
2025-09-05 16:36:11 +0900

    templui components update

--- cmd/web/components/ui/icon/icondata.go
+++ cmd/web/components/ui/icon/icondata.go
@@ -1,4 +1,4 @@
-// templui component icon - version: v0.95.0 installed by templui v0.95.0
+// templui component icon - version: v0.96.0 installed by templui v0.96.0
 package icon
 
 // This file is auto generated
